13ca59aafe47e693d3a0d2437e35370475209c58
arthiatsr <arthiatsr@users.noreply.github.com>
2019-11-26 23:57:37 -0600

    deploying in heroku

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var PORT = process.env.PORT || 8080;
+var path = require("path");
+var PORT = process.env.PORT || 3000;
 
 var app = express();
-var port = 3000;
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
